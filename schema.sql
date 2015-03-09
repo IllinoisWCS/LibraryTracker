a3806198f73d70b537e472e188dbf893201545d6
woohooyayaya <che12@illinois.edu>
2015-03-09 00:25:37 -0500

    reset

--- schema.sql
+++ schema.sql
@@ -10,7 +10,8 @@ create table books (
   id integer primary key autoincrement,
   name text not null,
   authors text not null,
-  available text not null,
+  copies integer not null,
+  available integer not null,
   cover text not null,
   year integer,
   category text not null
@@ -19,5 +20,6 @@ create table books (
 drop table if exists requests;
 create table requests(
 bookname text primary key not null,
-
-)
+category text not null,
+author text
+);
